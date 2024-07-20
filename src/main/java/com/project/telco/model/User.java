6f71737fb357c008dc5f7d6232cdee5c6c8ceabe
YNimantha <2020cs205@stu.ucsc.cmb.ac.lk>
2024-07-20 19:57:49 +0530

    added save method

--- src/main/java/com/project/telco/model/User.java
+++ src/main/java/com/project/telco/model/User.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class User {
+    private long userId;
     private String phonenumber;
     private String name;
     private int age;
--- src/main/java/com/project/telco/repository/JdbcUserRepository.java
+++ src/main/java/com/project/telco/repository/JdbcUserRepository.java
@@ -2,10 +2,16 @@ package com.project.telco.repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Arrays;
 
+import org.hibernate.sql.exec.spi.JdbcOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.project.telco.model.User;
@@ -13,9 +19,11 @@ import com.project.telco.model.User;
 @Repository
 public class JdbcUserRepository implements UserRepository{
     private JdbcTemplate jdbcTemplate;
+    private JdbcOperations jdbcOperations;
 
-    public JdbcUserRepository(JdbcTemplate jdbcTemplate) {
+    public JdbcUserRepository(JdbcTemplate jdbcTemplate, JdbcOperations jdbcOperations) {
         this.jdbcTemplate = jdbcTemplate;
+        this.jdbcOperations = jdbcOperations;
 
     }
 
@@ -30,15 +38,29 @@ public class JdbcUserRepository implements UserRepository{
         PreparedStatementCreatorFactory pscf = 
             new PreparedStatementCreatorFactory(
                 "insert into Taco_Order " 
-                + "()" 
+                + "(name, phonenumber, age) values(?, ?, ? )", 
+                Types.VARCHAR, Types.VARCHAR, Types.VARCHAR
             );
+        pscf.setReturnGeneratedKeys(true);
+        
+        PreparedStatementCreator psc = pscf.newPreparedStatementCreator(
+            Arrays.asList(
+                user.getName(),
+                user.getPhonenumber(),
+                user.getAge()
+            )
+        );
+
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcOperations.update(psc,keyHolder);
+        long userId = keyHolder.getKey().longValue();
+        user.setUserId(userId);
         return user;
     }
 
 
     private User mapRowToUser(ResultSet row, int rowNum) 
         throws SQLException {
-            String id = row.getString("id");
             String phoneNumber = row.getString("phonenumber");
             String name = row.getString("name");
             int age = row.getInt("age");
