3fa9135428e04fa0a5f637147e9b0eb29b297db2
YNimantha <2020cs205@stu.ucsc.cmb.ac.lk>
2024-07-21 17:23:31 +0530

    added save method

--- src/main/java/com/project/telco/controller/ServiceController.java
+++ src/main/java/com/project/telco/controller/ServiceController.java
@@ -2,19 +2,25 @@ package com.project.telco.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.project.telco.model.User;
 import com.project.telco.repository.UserRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
 @RequestMapping("/api")
 //Maintain variable in the session
-// @SessionAttributes("user")
+@SessionAttributes("user")
 public class ServiceController {
     private UserRepository userRepository;
     //make sure user is in the session
@@ -38,6 +44,17 @@ public class ServiceController {
         System.out.println(user );
    }
 
+   @RequestMapping(value  ="/addUser", method=RequestMethod.POST)
+   public void setUser(@RequestBody User user, Errors errors, SessionStatus sessionStatus) {
+        if(errors.hasErrors()) {
+            System.out.println("failed");
+        }
+
+        System.out.println(user);
+      //userRepository.save(user);
+        //sessionStatus.setComplete();
+   }
+   
    //Model is maintained throughout the session
    @RequestMapping(value = "/model", method=RequestMethod.GET)
    public void getModel(Model model) {
