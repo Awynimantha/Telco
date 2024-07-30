078bda4565aebb7691694f7119c225ae23e543fe
Awynimantha <awynimantha@gmail.com>
2024-07-30 23:11:01 +0530

    added login user

--- src/main/java/com/project/telco/security/SecurityConfig.java
+++ src/main/java/com/project/telco/security/SecurityConfig.java
@@ -9,27 +9,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-
     }
 
-    @Bean 
+    @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        List<UserDetails>  usersList = new ArrayList<>();
+        List<UserDetails> usersList = new ArrayList<>();
         usersList.add(new User(
-            "User1", encoder.encode("password"), 
+            "User1", encoder.encode("password"),
             Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))
         ));
-        return (UserDetailsService) new InMemoryUserDetailsManager(usersList);
+        return new InMemoryUserDetailsManager(usersList);
     }
-    
 }
