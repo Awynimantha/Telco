d639e6511d0459958e209fe1aaff10b7df673049
Awynimantha <awynimantha@gmail.com>
2024-08-07 22:43:26 +0530

    fixed database creation

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS "Userdfd" (
-    id identity PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     phonenumber VARCHAR(15) NOT NULL,
     age INT NOT NULL CHECK (age >= 0)
